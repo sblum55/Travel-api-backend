b38ed267c562b55f83da382e56a0fe9a059fa720
sblum55 <healysa55@gmail.com>
2021-04-09 11:58:25 -0400

    add folders and started server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const rowdy = require('rowdy-logger')
+const routesReport = rowdy.begin(app)
+
+app.use(express.json())
+app.use(require('cors')())
+
+
+
+const PORT = process.env.port || 3001
+app.listen(PORT, () => {
+    console.log(`port running on ${PORT}`)
+  routesReport.print()
+})
