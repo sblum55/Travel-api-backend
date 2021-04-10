6985eecad43720fd257beac27fd78e39d8338a19
sblum55 <healysa55@gmail.com>
2021-04-10 18:08:07 -0400

    still working on saving country

--- routes/countryRoutes.js
+++ routes/countryRoutes.js
@@ -3,6 +3,6 @@ const countryControllers = require('../controllers/countryControllers')
 const countryRouter = express.Router()
 
 countryRouter.get('/:country', countryControllers.oneCountry)
-
+countryRouter.post('/:savedCountry/:userId', countryControllers.saveCountry)
 
 module.exports = countryRouter
